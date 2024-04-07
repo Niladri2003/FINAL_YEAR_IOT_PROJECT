6241ae9e937f37238565be42893695071a3772a0
Niladri2003 <niladri.adak.21@gmail.com>
2024-04-08 00:18:34 +0530

    server file changed

--- server/index.js
+++ server/index.js
@@ -63,3 +63,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+app.get("/",(req, res) => {
+    res.status(200).send({
+        sucess:true,
+        message:"Succesfully running server",
+    })
+})
